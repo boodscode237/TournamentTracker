5c10995006a5fa07b0c342b44cb273439a1a323e
boodscode237 <boodscode@gmail.com>
2022-03-10 01:02:01 +0500

    Create CreateRound methods unfinished

--- TrackerLibrary/TournamentLogic.cs
+++ TrackerLibrary/TournamentLogic.cs
@@ -12,15 +12,81 @@ namespace TrackerLibrary
         // Order our list of teams randomly
         // check if it is big enough - if not, add in byes 2*2*2*2 - 2^4
         // create our first round of matchups
-        //Create any rounds after that
+        // create any rounds after that
 
         public static void CreateRounds(TournamentModel model)
         {
             List<TeamModel> randomizeTeams = RandomizeTeamModel(model.EnteredTeams);
+            int rounds = FindNumberOfRounds(randomizeTeams.Count);
+            int byes = NumberOfByes(rounds, randomizeTeams.Count);
+
+
+            model.Rounds.Add(CreateFirstRound(byes, randomizeTeams));
+
+            CreateOtherRounds(model, rounds);
+        }
+
+        private static void CreateOtherRounds(TournamentModel model, int rounds)
+        {
+            int round = 2;
+            List<MatchupModel> previousRound = model.Rounds[0];
+        }
+
+        private static List<MatchupModel> CreateFirstRound(int byes, List<TeamModel> teams)
+        {
+            List<MatchupModel> output = new List<MatchupModel>();
+            MatchupModel curr = new MatchupModel();
+
+            foreach (TeamModel team in teams)
+            {
+                curr.Entries.Add(new MatchupEntryModel { TeamCompeting = team });
+                if (byes > 0 || curr.Entries.Count > 1)
+                {
+                    curr.MatchupRound = 1;
+                    output.Add(curr);
+                    curr = new MatchupModel();
+
+                    if (byes > 0)
+                    {
+                        byes -= 1;
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        private static int NumberOfByes(int rounds, int numberOfTeams)
+        {
+            int output = 0;
+            int totalTeams = 1;
+
+            for (int i = 1; i <= rounds; i++)
+            {
+                totalTeams *= 2;
+            }
+            output = totalTeams - numberOfTeams;
+
+            return output;
+        }
+
+        private static int FindNumberOfRounds(int teamCount)
+        {
+            int output = 1;
+            int val = 2;
+
+            while (val < teamCount)
+            {
+                output += 1;
+                val *= 2;
+            }
+
+            return output;
         }
 
         private static List<TeamModel> RandomizeTeamModel(List<TeamModel> teams)
         {
+            // randomnize List Items in C#
             return teams.OrderBy(x => Guid.NewGuid()).ToList();
         }
     }
